91a07adaf2af3de374fedb15843b2c83f895f930
Memona Amir <157165172+MemonaAmirAbdulHaq@users.noreply.github.com>
2025-06-21 14:43:43 +0500

    update login form

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -6,9 +6,13 @@ const Login = () => {
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
           Login to your account
-        </h2>
-           
+        </h2>  
         </div> 
+        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+            
+          </div>
+        </div>
          </div>
   )
 }
